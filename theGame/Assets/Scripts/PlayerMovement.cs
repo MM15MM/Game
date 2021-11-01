264903042fde0694c3144d02c6294f7dac5e8002
Lorenza <lor.carru@stud.uniroma3.it>
2021-11-01 15:13:21 +0100

    First commit
    Terzo livello: messo sfondo, personaggio e alcune piastrelle, da sistemare ancora salti, animazioni, da introdurre ancora spostamento di main camera, scrolling, nemici e trappole.

---
+++ theGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+    float speed;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        speed = 3.0f;
+
+        Vector2 position = transform.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + 1.5f * speed * vertical * Time.deltaTime;
+        transform.position = position;
+
+        anim.SetFloat("MoveX", speed * horizontal);
+        anim.SetFloat("MoveY", speed * vertical);
+    }
+}
