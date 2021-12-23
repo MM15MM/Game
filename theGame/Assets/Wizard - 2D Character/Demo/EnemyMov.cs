c708f8d31cada67cc5263d883b2a56fc07f75234
Lorenza <lor.carru@stud.uniroma3.it>
2021-12-23 17:49:59 +0100

    Problema da risolvere

---
+++ theGame/Assets/Wizard - 2D Character/Demo/EnemyMov.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMov : MonoBehaviour
+{
+    public float speed;
+    public bool moveRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveRight)
+        {
+            transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+            transform.localScale = new Vector2(1,1);
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+            transform.localScale = new Vector2(-1, 1);
+        }
+    }
+}
