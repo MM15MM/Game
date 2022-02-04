0991810a21e04d240666bfcffff9a8ae6f676136
MM15MM <marinabilleci01@gmail.com>
2022-02-04 17:53:20 +0100

    Unione scene

---
+++ Game/Assets/Wizard - 2D Character/Demo/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using ClearSky;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 15;
+    public Animator deathAnim;
+
+    void Update()
+    {
+
+    }
+    public void TakeDamage(int damage)
+    {
+        deathAnim.SetBool("Hurt", true);
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+            Destroy(gameObject);
+        }
+        deathAnim.SetBool("Hurt", false);
+    }
+
+    void Die()
+    {
+        deathAnim.SetBool("isDead", true);
+    }
+}
