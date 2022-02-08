0b0adbf8a90205eea031cc0001ddc48bc46866e4
MM15MM <marinabilleci01@gmail.com>
2022-02-08 15:51:47 +0100

    aggiunto dialogo

---
+++ Game/Assets/Parte2AndDialogo/dialogueAssets/scriptsDia/LoadParte1.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadParte1 : MonoBehaviour
+{
+    
+   public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+   
+}
